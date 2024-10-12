56602aa1c16f51e628ea8dac7da1f79157118a3f
vishal <lahsiv250601@gmail.com>
2024-10-12 20:30:41 +0530

    added decode functions

---
+++ src/JwtDecoder.js
@@ -0,0 +1,32 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const jwtPublicKey = process.env.JWT_PUBLIC_KEY.replace(/\\n/g, '\n');
+
+async function decodeJwt(token) {
+    return new Promise((resolve, reject) => {
+        jwt.verify(token, jwtPublicKey, function(error, decoded) {
+            if (error) reject({message : error.message});
+            resolve(decoded);
+        });
+    })
+}
+
+async function decodeJwtFlexible(token){
+    return new Promise((resolve, reject) => {
+        jwt.verify(token, jwtPublicKey, function(error, decoded) {
+            if (error) {
+                if (error.name === 'TokenExpiredError') {
+                    const decodedData = jwt.decode(token); // Decode without verification
+                    resolve(decodedData);
+                } else {
+                    reject({message : error.message});
+                }
+            } else {
+                resolve(decoded);
+            }
+        })
+    })
+}
+
+module.exports.decodeJwt = {decodeJwt}
+module.exports.decodeJwtFlexible = {decodeJwtFlexible}
