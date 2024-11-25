0b597f220d8891c756adced83dab4121633a37cd
vishal <lahsiv250601@gmail.com>
2024-11-25 14:19:59 +0530

    added role validation in certify auth

--- src/JwtDecoder.js
+++ src/JwtDecoder.js
@@ -45,4 +45,28 @@ async function validateAuth(bearer){
     }
 }
 
+async function validateAuthWithRoles(bearer, grantedRoles){
+    const token = bearer && bearer.startsWith("Bearer ") ? bearer.slice(7) : bearer;
+    if (token) {
+        try {
+            const tokenData =  await decodeJwt(token);
+            if (!grantedRoles.includes(tokenData.role)) {
+                const e =  new Error('Unauthorized. You do not have the required role.')
+                e.code = 403
+                throw e;
+            }else{
+                return tokenData;
+            }
+        }catch (error){
+            const e =  new Error(error.message)
+            e.code = 401
+            throw e;
+        }
+    }else {
+        const e =  new Error('No token provided.')
+        e.code = 400
+        throw e;
+    }
+}
+
 module.exports = { decodeJwt, decodeJwtFlexible, validateAuth };
